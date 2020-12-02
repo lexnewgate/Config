48fcc73a426c008b067e53862a37e98c50128bf4
LittleCopernicus <weiwuhao@qq.com>
2020-12-03 00:02:24 +0800

    upate my shell config

--- .myshellconfig.sh
+++ .myshellconfig.sh
@@ -1,2 +1,13 @@
+#githug 
+alias ggplay="githug play"
+alias ggreset="githug reset"
+alias gglevels="githug levels"
+alias gghints="githug hint"
+
+
 export VIMCONFIG=~/.config/nvim 
 export VIMDATA=~/.local/share/nvim
+
+
+alias zshconfig="code ~/.zshrc"
+
